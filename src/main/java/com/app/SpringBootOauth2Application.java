c43ca92e33ac98304f22c04f5b50e1388f5b0310
VIJAY <VIJAY@DESKTOP-MFHD1SP>
2020-09-08 13:36:44 +0530

    Oauth2 First Commit

---
+++ src/main/java/com/app/SpringBootOauth2Application.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@EnableOAuth2Sso
+@SpringBootApplication
+public class SpringBootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2Application.class, args);
+	}
+
+}
